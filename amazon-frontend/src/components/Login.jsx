a6f699e7e8b6dd969c2a31cf8800231d721cb9c0
saravana3252 <sara18ec118@gmail.com>
2025-02-05 18:10:50 +0530

    login & register updated

--- amazon-frontend/src/components/Login.jsx
+++ amazon-frontend/src/components/Login.jsx
@@ -3,6 +3,8 @@ import {Link, useNavigate} from "react-router-dom"
 import auth from "../config";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { userContext } from "./context/userContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 
 function Login(){
@@ -13,6 +15,7 @@ const [message,setMessage] = useState({
     text:"",
     type:""
 })
+const [isLoading,setIsLoading] = useState(false)
 
 const navigate = useNavigate()
 
@@ -32,6 +35,7 @@ useEffect(()=>{
 
 function handleLogin(e){
     e.preventDefault()
+    setIsLoading(true)
     signInWithEmailAndPassword(auth,email,password).then((userCreds)=>{
         const user = userCreds.user
         const usercredentials = {
@@ -46,21 +50,38 @@ function handleLogin(e){
 
     }).catch((err)=>{
         setMessage({type:"error",text:err.message})
+    }).finally(()=>{
+        setIsLoading(false)
     })
 }
 
     return(
         <>
-        <div className="bg-white h-screen w-full flex flex-col items-center pt-10">
+        <div className="bg-white h-screen w-full flex flex-col pt-24 p-5 lg:p-0 items-center lg:pt-10">
            <img src="./amazon-logo2.png" className="h-20"></img>
            <div className="w-full max-w-md bg-white border border-black p-4">
            <form >
             <h1 className="font-bold text-2xl text-center">Sign in Your Account</h1>
             <div className="flex flex-col gap-3 pt-4">
-            <input name="email"  type="email" className="p-2 border border-black" placeholder="enter your email" required onChange={(e)=>{setEmail(e.target.value)}}></input>
-            <input name="password" type="password" className="p-2 border border-black" placeholder="enter your password" required onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <input name="email"  type="email" className="p-2 border border-black outline-none" placeholder="enter your email" required onChange={(e)=>{setEmail(e.target.value)}}></input>
+            <div className="flex border border-black">
+            <input id="password" name="password" type="password" className="p-2 outline-none w-[80%]" placeholder="enter your password" required onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <button type="button" className="p-2 bg-gray-300 cursor-pointer border-l border-black w-[20%]" onMouseDown={(e)=>{ 
+                e.preventDefault()
+                document.getElementById("password").type = "text"
+            }} onMouseUp={(e)=>{
+                e.preventDefault()
+                document.getElementById("password").type = "password"
+            }} onTouchStart={(e)=>{ 
+                e.preventDefault()
+                document.getElementById("password").type = "text"
+            }} onTouchEnd={(e)=>{
+                e.preventDefault()
+                document.getElementById("password").type = "password"
+            }}><FontAwesomeIcon icon={faEye} /></button>
+            </div>
             <div className="flex flex-col gap-3 items-center mt-1">
-            <button className="p-2 bg-yellow-400 w-[50%] font-semibold" onClick={handleLogin}>SIGN IN</button>
+            <button className="p-2 bg-yellow-400 w-[50%] font-semibold" onClick={handleLogin}>{isLoading ? "LOADING..." : "SIGN IN"}</button>
             <p>DONT HAVE AN ACCOUNT?<span className="font-bold"> <Link to="/register">REGISTER</Link> </span></p>
             <p className={`${message.type} p-2 px-4 mt-2 font-medium text-lg `}>{message.text}</p>
             </div>
--- amazon-frontend/src/components/Register.jsx
+++ amazon-frontend/src/components/Register.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import {Link} from "react-router-dom"
 import auth from "../config";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 function Register(){
 
@@ -12,10 +14,11 @@ function Register(){
         type:"",
         text:""
     })
+    const [isLoading,setIsLoading] =useState(false)
 
     function handleRegister(e) {
         e.preventDefault();
-
+        setIsLoading(true)
         createUserWithEmailAndPassword(auth, email, password)
             .then((userCred) => {
                 const user = userCred.user;
@@ -27,6 +30,9 @@ function Register(){
                         setEmail("")
                         setPassword("")
                         console.log("User Name:", user.displayName);
+                        setTimeout(()=>{
+                            setMessage({type:"invisible",text:"dummy text"})
+                        },4000)
                     })
                     .catch((error) => {
                         setMessage({type:"error",text:error.message})
@@ -36,21 +42,38 @@ function Register(){
             .catch((error) => {
                 setMessage({type:"error",text:error.message})
                 console.error("Error creating user:", error.message);
-            });
+            }).finally(()=>{
+                setIsLoading(false)
+            })
     }
     return(
         <>
-        <div className="bg-white h-screen w-full flex flex-col items-center pt-10">
+        <div className="bg-white h-screen w-full flex flex-col items-center pt-20 p-5 lg:p-0 lg:pt-10">
            <img src="./amazon-logo2.png" className="h-20"></img>
            <div className="w-full max-w-md bg-white border border-black p-4">
            <form>
             <h1 className="font-bold text-2xl text-center">Register Your Account</h1>
             <div className="flex flex-col gap-3 pt-4">
-            <input name="name" type="text" className="p-2 border border-black" placeholder="enter your name" required value={name}  onChange={(e)=>{setName(e.target.value)}}></input>
-            <input name="email" type="email" className="p-2 border border-black" placeholder="enter your email" required value={email} onChange={(e)=>{setEmail(e.target.value)}}></input>
-            <input name="password" type="password" className="p-2 border border-black" placeholder="enter your password" value={password} onChange={(e)=>{setPassword(e.target.value)}} required></input>
+            <input name="name" type="text" className="p-2 border border-black outline-none" placeholder="enter your name" required value={name}  onChange={(e)=>{setName(e.target.value)}}></input>
+            <input name="email" type="email" className="p-2 border border-black outline-none" placeholder="enter your email" required value={email} onChange={(e)=>{setEmail(e.target.value)}}></input>
+             <div className="flex border border-black">
+                        <input id="password" name="password" type="password" className="p-2 outline-none w-[80%]" placeholder="enter your password" value={password} required onChange={(e)=>{setPassword(e.target.value)}}></input>
+                        <button type="button" className="p-2 bg-gray-300 cursor-pointer border-l border-black w-[20%]" onMouseDown={(e)=>{ 
+                            e.preventDefault()
+                            document.getElementById("password").type = "text"
+                        }} onMouseUp={(e)=>{
+                            e.preventDefault()
+                            document.getElementById("password").type = "password"
+                        }} onTouchStart={(e)=>{ 
+                            e.preventDefault()
+                            document.getElementById("password").type = "text"
+                        }} onTouchEnd={(e)=>{
+                            e.preventDefault()
+                            document.getElementById("password").type = "password"
+                        }}><FontAwesomeIcon icon={faEye} /></button>
+            </div>
             <div className="flex flex-col gap-3 items-center mt-1">
-            <button className="p-2 bg-yellow-400 w-[50%] font-semibold" onClick={handleRegister}>Create Account</button>
+            <button className="p-2 bg-yellow-400 w-[50%] font-semibold" onClick={handleRegister}>{isLoading ? "Loading..." : "Create Account"}</button>
             <p>DONT HAVE AN ACCOUNT?<span className="font-bold"> <Link to="/login">LOGIN</Link> </span></p>
             <p className={`${message.type} p-2 px-4 mt-2 font-medium text-lg `}>{message.text}</p>
             </div>
