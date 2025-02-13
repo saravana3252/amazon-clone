acd1039a1249de27b3bff8a01bda5b4a9999bef3
saravana3252 <sara18ec118@gmail.com>
2025-02-13 18:24:20 +0530

    ui updated

--- amazon-frontend/src/components/AdminProductList.jsx
+++ amazon-frontend/src/components/AdminProductList.jsx
@@ -64,7 +64,7 @@ function ProductList(){
 
     return(
         <>
-         <div className="bg-gray-300 min-h-screen">
+         <div className="bg-gray-300 h-full">
             <div className="h-20 bg-gray-600 text-white  lg:flex space-x-5 items-center px-5 hidden">
                 <p className="cursor-pointer transition-all duration-300 hover:bg-gray-500 p-2 px-4 rounded" onClick={()=>{setProducts(originalProds)}}>All</p>
                 <p className="cursor-pointer transition-all duration-300 hover:bg-gray-500 p-2 px-4 rounded" onClick={handleGamingProds}>Gaming</p>
@@ -88,7 +88,7 @@ function ProductList(){
                 </div>
             
             {
-                isLoading ? (<><p className="text-center p-10">loading...</p> </>):(<>    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:gap-5 gap-2 mb-10 p-5">
+                isLoading ? (<><p className="text-center p-10">loading...</p> </>):(<>    <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-2 lg:gap-5 gap-2 mb-10 p-5 min-h-screen">
 
                     {
                         products.map((product)=>{
