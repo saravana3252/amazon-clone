86bca7979b1ee0c117b48df139b0807476894635
saravana3252 <sara18ec118@gmail.com>
2025-02-13 14:57:51 +0530

    frontend updated

--- amazon-frontend/src/components/PrivateCheckout.jsx
+++ amazon-frontend/src/components/PrivateCheckout.jsx
@@ -5,12 +5,12 @@ import { Navigate } from "react-router-dom"
 function PrivateCheckout(props){
 
     const loggedInData =useContext(userContext)
-    const {Component,...rest,cartData} = props
+    const {Component,cartData,...rest} = props
 
     return (
         <>
         {
-           loggedInData.loggedUser !== null && cartData.length > 0 ? (<Component {...rest} ></Component>):(<Navigate to="/"></Navigate>)
+           loggedInData.loggedUser !== null && cartData && cartData.length > 0 ? (<Component {...rest} ></Component>):(<Navigate to="/"></Navigate>)
         }
         
         </>
