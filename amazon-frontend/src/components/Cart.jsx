65fb01f72a006d9d562f6a8bd3bdf5ff34d91ba2
saravana3252 <sara18ec118@gmail.com>
2025-02-15 10:49:19 +0530

    ui updated

--- amazon-frontend/src/components/Cart.jsx
+++ amazon-frontend/src/components/Cart.jsx
@@ -110,7 +110,7 @@ function Cart(props) {
                                         </div>
 
                                         <button
-                                            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition"
+                                            className="px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 cursor-pointer transition"
                                             onClick={() => props.RemoveFromCart(index)}
                                         >
                                             Remove
